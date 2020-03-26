aae4b5ef61cd63ea2e1d80b0569717a4d02f5690
Ramzi Charradi <ramzi.charradi2@gmail.com>
2020-03-26 01:15:53 +0100

    added retinanet training

--- Detection_task/Detection_task/main.py
+++ Detection_task/Detection_task/main.py
@@ -8,7 +8,11 @@ from src import utils
 from src.plots import plot_prediction, plot_random_batch
 
 
+import numpy as np
+import collections
+import torch.optim as optim
 
+from src import model
 
 
 if __name__ == "__main__":
@@ -33,11 +37,11 @@ if __name__ == "__main__":
     torch.manual_seed(1)
 
     data_loader = torch.utils.data.DataLoader(
-                train_set, batch_size=2, shuffle=True, num_workers=0,
+                train_set, batch_size=1, shuffle=True, num_workers=0,
                 collate_fn= utils.collate_fn)
 
     val_data_loader = torch.utils.data.DataLoader(
-                valid_set, batch_size=2, shuffle=True, num_workers=0,
+                valid_set, batch_size=1, shuffle=True, num_workers=0,
                 collate_fn= utils.collate_fn)
     
     #image_mean = utils.compute_means(train_set)
@@ -108,3 +112,91 @@ if __name__ == "__main__":
     plot_prediction(img, OD_true_box, Fovea_true_box, OD_predicted_box, Fovea_predicted_box)     
 #%%
     print(evaluate_mean_distance(model, valid_set))
+#%% 
+#########################################################################"#
+    ############             RetinaNet                       #############""
+    ########################################################################"
+    
+    
+    
+
+    depth = 50
+    
+    epochs = 10
+    # Create the model
+    if depth == 18:
+        retinanet = model.resnet18(num_classes=3, pretrained=True)
+    elif depth == 34:
+        retinanet = model.resnet34(num_classes=3, pretrained=True)
+    elif depth == 50:
+        retinanet = model.resnet50(num_classes=3, pretrained=True)
+    elif depth == 101:
+        retinanet = model.resnet101(num_classes=3, pretrained=True)
+    elif depth == 152:
+        retinanet = model.resnet152(num_classes=3, pretrained=True)
+
+    retinanet = retinanet.cuda()
+    retinanet = torch.nn.DataParallel(retinanet).cuda()
+    retinanet.training = True
+
+    optimizer = optim.Adam(retinanet.parameters(), lr=1e-5)
+
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
+
+    loss_hist = collections.deque(maxlen=500)
+
+    retinanet.train()
+    retinanet.module.freeze_bn()
+
+
+    for epoch_num in range(epochs):
+        min_loss = 10
+        retinanet.train()
+        retinanet.module.freeze_bn()
+        epoch_loss = []
+        for iter_num in range(train_set.__len__()):
+            
+            img,target,_ = train_set[iter_num]
+            annotations = utils.get_annotations_retinanet(target)
+            img = img.unsqueeze(0)
+            optimizer.zero_grad()
+            classification_loss, regression_loss = retinanet([ img.cuda(), annotations.cuda()])
+            try:
+
+                classification_loss = classification_loss.mean()
+                regression_loss = regression_loss.mean()
+                loss = classification_loss + regression_loss
+                if bool(loss == 0):
+                    continue
+
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(retinanet.parameters(), 0.1)
+                optimizer.step()
+                loss_hist.append(float(loss))
+                epoch_loss.append(float(loss))
+                if min_loss > float(loss):
+                    min_loss = float(loss)
+                    torch.save(retinanet, "retinanet_5.pt")
+                    print("Model saved")
+                    
+                if iter_num % 10 == 0 :
+                    print(
+                            'Epoch: {} | Iteration: {} | Classification loss: {:1.5f} | Regression loss: {:1.5f} | Running loss: {:1.5f}'.format(
+                                    epoch_num, iter_num, float(classification_loss), float(regression_loss), np.mean(loss_hist)))
+                del classification_loss
+                del regression_loss
+            except Exception as e:
+                print("lz")
+                print(e)
+                continue
+#%%
+    model = torch.load("retinanet.pt")
+#%%
+    model.eval()
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    evaluate(model, valid_set, device)
+    
+#%%
+    img, OD_true_box, Fovea_true_box, OD_predicted_box, Fovea_predicted_box = utils.get_boxes(model, valid_set, threshold = 0.008, img_idx = 0, model_type = "retinanet")
+        
+    plot_prediction(img, OD_true_box, Fovea_true_box, OD_predicted_box, Fovea_predicted_box)  
